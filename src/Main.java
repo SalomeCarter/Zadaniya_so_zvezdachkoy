e3a9bbf961c46b6f08edb0aaebc196cb1c9a0261
SalomeCarter <elizabethboston@icloud.com>
2022-12-24 16:59:27 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+
+
+// 1. ВЫВЕСТИ ЧИСЛО В ОБРАТНОМ ПОРЯДКЕ
+// public class Main {
+
+//        public static void main(String[] args) {
+//            int[] arr = {1, 2, 3, 4, 5};
+//
+//            for (int i = arr.length - 1; i >= 0 ; i--) {
+//                System.out.println(arr[i]);
+
+
+// 2. МАКСИМАЛЬНЫЕ И МИНИМАЛЬНЫЕ ЧИСЛА
+//    class Test {
+//
+//        public static void main(String[] args) {
+//
+//            float num1 = 4.25f;
+//            int num2 =5;
+//
+//            System.out.println("Max number: " + Math.max(num1,num2));
+//            System.out.println("Min number: " + Math.min(num1,num2));
+//        }
+//    }
+
+
+
